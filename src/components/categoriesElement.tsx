d6b91fa228a4142a305e268275db8465df2c617c
Colin <colin.heggli@gmail.com>
2024-02-26 23:43:01 +0100

    data.json

--- src/components/categoriesElement.tsx
+++ src/components/categoriesElement.tsx
@@ -5,14 +5,26 @@ const CategoriesElement = ({categories}: { categories: { [key: string]: SiteInte
     return (
         <>
             {
-                Object.keys(categories).length === 0 ? <h1 className={"text-center text-2xl font-semibold mt-4"}>No sites found</h1> :
-                Object.keys(categories).map((category: string, index: number) => (
-                    <CategoryElement
-                        key={index}
-                        sitesData={categories[category]}
-                        categoryName={category}
-                    />
-                ))
+                Object.keys(categories).length === 0 ?
+                    <div
+                        className={"flex flex-col gap-4 items-center justify-center w-full ease-in-out duration-200 transition-colors mt-32 opacity-70"}
+                    >
+                        <h1 className={"text-center text-2xl font-semibold mt-4 text-darkgray-50"}>No sites found</h1>
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                             className="h-32 aspect-square text-darkgray-50 mx-auto mt-4 fill-darkgray-50"
+                             viewBox="0 0 512 512">
+                            <path
+                                d="M88 0C74.7 0 64 10.7 64 24c0 38.9 23.4 59.4 39.1 73.1l1.1 1C120.5 112.3 128 119.9 128 136c0 13.3 10.7 24 24 24s24-10.7 24-24c0-38.9-23.4-59.4-39.1-73.1l-1.1-1C119.5 47.7 112 40.1 112 24c0-13.3-10.7-24-24-24zM32 192c-17.7 0-32 14.3-32 32V416c0 53 43 96 96 96H288c53 0 96-43 96-96h16c61.9 0 112-50.1 112-112s-50.1-112-112-112H352 32zm352 64h16c26.5 0 48 21.5 48 48s-21.5 48-48 48H384V256zM224 24c0-13.3-10.7-24-24-24s-24 10.7-24 24c0 38.9 23.4 59.4 39.1 73.1l1.1 1C232.5 112.3 240 119.9 240 136c0 13.3 10.7 24 24 24s24-10.7 24-24c0-38.9-23.4-59.4-39.1-73.1l-1.1-1C231.5 47.7 224 40.1 224 24z"/>
+                        </svg>
+                    </div>
+                    :
+                    Object.keys(categories).map((category: string, index: number) => (
+                        <CategoryElement
+                            key={index}
+                            sitesData={categories[category]}
+                            categoryName={category}
+                        />
+                    ))
             }
         </>
     )
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,6 +4,9 @@ const Header = () => {
             <div className="flex justify-between items-center border-b-[1px] border-darkgray-700 text-darkgray-0 p-4 fixed top-0 right-0 left-0 backdrop-blur">
                 <div className="text-2xl font-bold">StreamSites</div>
                 <div className="flex gap-4">
+                    <a href="https://github.com/ydfdas1f546g1df/streamSites/blob/b082cbe263e25399aa6b86f1a5ad7690f238aa4e/src/data/data.json" target={"_blank"} className="text-darkgray-100 hover:text-darkgray-50 font-semibold">
+                        data.json
+                    </a>
                     <a href="https://github.com/ydfdas1f546g1df/streamSites" target={"_blank"} className="text-darkgray-100 hover:text-darkgray-50 font-semibold">
                         GitHub
                     </a>

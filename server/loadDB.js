d28d32c693eccd7f54c9e6c7d4ca1f0eba3533d9
M4rshe1 <colin.heggli@gmail.com>
2024-03-02 20:28:02 +0100

    db init fix

--- server/loadDB.js
+++ server/loadDB.js
@@ -9,7 +9,7 @@ function LoadDB(callback) {
         }
         console.log('Connected to the database.');
     });
-    
+
     // check if the database is already populated
     db.get(`SELECT * FROM tbl_sites`, (err, row) => {
         if (err) {
@@ -18,46 +18,39 @@ function LoadDB(callback) {
         }
         if (row) {
             console.log('Database already populated.');
-            db.close((err) => {
-                if (err) {
-                    console.error(err.message);
-                }
-                console.log('Closed the database connection.');
-                callback();
+        } else {
+            console.log('Database not populated. Populating database...');
+            const fileData = fs.readFileSync(path.join(process.cwd(), 'data', 'data.json'));
+            const data = JSON.parse(fileData);
+            const languages = [...new Set(data.map(site => site.languages).flat())];
+            const categories = [...new Set(data.map(site => site.category))];
+
+            db.serialize(() => {
+                languages.forEach(language => {
+                    db.run(`INSERT INTO tbl_languages (name) VALUES (?)`, [language]);
+                });
+
+                categories.forEach(category => {
+                    db.run(`INSERT INTO tbl_categories (name) VALUES (?)`, [category]);
+                });
+
+                data.forEach(site => {
+                    db.run(`INSERT INTO tbl_sites (name, url, icon, category_fk) VALUES (?, ?, ?, (SELECT pk_categories FROM tbl_categories WHERE name = ?))`, [site.name, site.url, site.icon, site.category]);
+                    site.languages.forEach(language => {
+                        db.run(`INSERT INTO tbl_sites_languages (site_fk, language_fk) VALUES ((SELECT pk_sites FROM tbl_sites WHERE name = ?), (SELECT pk_languages FROM tbl_languages WHERE name = ?))`, [site.name, language]);
+                    });
+                });
+                console.log('Data loaded into database.');
             });
-            return;
+            
         }
-        console.log('Database not populated. Populating database...');
-    });
-
-    const fileData = fs.readFileSync(path.join(process.cwd(), 'data', 'data.json'));
-    const data = JSON.parse(fileData);
-    const languages = [...new Set(data.map(site => site.languages).flat())];
-    const categories = [...new Set(data.map(site => site.category))];
-
-    db.serialize(() => {
-        languages.forEach(language => {
-            db.run(`INSERT INTO tbl_languages (name) VALUES (?)`, [language]);
+        db.close((err) => {
+            if (err) {
+                console.error(err.message);
+            }
+            console.log('Closed the database connection.');
+            callback();
         });
-
-        categories.forEach(category => {
-            db.run(`INSERT INTO tbl_categories (name) VALUES (?)`, [category]);
-        });
-
-        data.forEach(site => {
-            db.run(`INSERT INTO tbl_sites (name, url, icon, category_fk) VALUES (?, ?, ?, (SELECT pk_categories FROM tbl_categories WHERE name = ?))`, [site.name, site.url, site.icon, site.category]);
-            site.languages.forEach(language => {
-                db.run(`INSERT INTO tbl_sites_languages (site_fk, language_fk) VALUES ((SELECT pk_sites FROM tbl_sites WHERE name = ?), (SELECT pk_languages FROM tbl_languages WHERE name = ?))`, [site.name, language]);
-            });
-        });
-        console.log('Data loaded into database.');
-    });
-    db.close((err) => {
-        if (err) {
-            console.error(err.message);
-        }
-        console.log('Closed the database connection.');
-        callback();
     });
 }
 

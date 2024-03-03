3226bbd419a3083db41d7731407ce9fb8e2b3c6c
M4rshe1 <colin.heggli@gmail.com>
2024-03-03 15:14:57 +0100

    readme fix

---
+++ README.md
@@ -0,0 +1,41 @@
+- <a href="https://streamSites.heggli.dev">Web Page</a>
+- <a href="./SITES.md">MD version</a>
+
+## Description
+
+This is a simple web page that lists all bunch of free streaming, movies, series, and anime sites. The web page is built
+with React and hosted with Docker.
+
+## Features
+
+- [x] List of free streaming sites
+- [x] Search bar
+- [x] Filter by category
+- [x] Filter by language
+- [x] Statistics
+- [ ] Filter by status
+- [ ] Filter by quality
+
+## Installation
+
+1. Clone the repository
+2. Create a .env file in the root directory and add the following environment variables:
+
+```
+VITE_API_URL=<API_URL>
+```
+
+3. Create a .env file in the server directory and add the following environment variables:
+
+```
+ADMIN_USER=<ADMIN_USER>
+ADMIN_PW=<ADMIN_PW>
+```
+
+4. Run `docker-compose up --build` in the root directory
+5. Open your browser and go to `http://localhost:3000`
+6. Done!
+
+## Contributing
+- [M4rshe1](https://github.com/M4rshe1)
+- [ydfdas1f546g1df](https://github.com/ydfdas1f546g1df)
--- src/components/toolTipElement.tsx
+++ src/components/toolTipElement.tsx
@@ -4,19 +4,18 @@ const ToolTipElement = ({toolTip}: { toolTip: string }) => {
     return (
         <div
             className="
-                        delay-150 scale-0 group-hover:scale-100 absolute bg-darkgray-0
+                        delay-50 scale-0 group-hover:scale-100 absolute bg-darkgray-0
                     text-darkgray-900 p-2 rounded transition-all min-h-full
                     border-darkgray-100 border-[1px]
                     duration-300 ease-in-out transform z-10
-                    -bottom-1/4 left-0 -translate-x-1/2 translate-y-full
-                    text-[11px] whitespace-nowrap font-semibold"
+                    -bottom-1/4 left-1 -translate-x-1/2 translate-y-full
+                    text-[11px] whitespace-nowrap font-semibold shadow-md opacity-0 group-hover:opacity-100"
         >
             {toolTip}
             <span
-                className="z-20 absolute w-4 aspect-square bg-darkgray-0 transform rotate-45 top-1 left-1/2 translate-x-1/2 -translate-y-1/2 rounded-sm"
+                className="absolute w-3 aspect-square bg-darkgray-0 transform rotate-45 top-1 left-1/2 translate-x-1/2 -translate-y-1/2 rounded-sm"
             >
-                            
-                        </span>
+            </span>
         </div>
     )
 }
